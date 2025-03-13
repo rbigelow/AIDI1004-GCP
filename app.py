fd1010aafe5e239859033ae8ebb5bf2b87a6847e
Ross Bigelow <ross@bigelow.cc>
2025-03-13 16:37:15 -0400

    created simple flask application.

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Home Page!"
+
+@app.route('/about')
+def about():
+    return "This is the About Page."
+
+@app.route('/contact')
+def contact():
+    return "This is the Contact Page."
+
+if __name__ == '__main__':
+    app.run(debug=True)
